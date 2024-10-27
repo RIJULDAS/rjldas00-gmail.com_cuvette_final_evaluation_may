300804e61479ee63865e3a99260f88257bfa2a54
RIJULDAS <rjldas00@gmail.com>
2024-10-27 17:12:01 +0530

    added files

--- frontend/src/components/ui/index.js
+++
@@ -1,6 +0,0 @@
-import Text from './Text';
-import Button from './Button';
-import Modal from './Modal';
-import Badge from './Badge';
-
-export { Text, Button, Modal, Badge };
--- frontend/src/constants.js
+++
@@ -1,1 +0,0 @@
-export const SECONDARY_COLOR = '#767575';
